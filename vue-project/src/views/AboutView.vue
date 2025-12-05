05ae644b74e3afeb9ee995c846b8004aea917282
E. K. Lee <wolf@teoal.net>
2025-12-06 01:07:04 +0900

    5장 vue 기본 문법 실습 완료

--- vue-project/src/views/AboutView.vue
+++ vue-project/src/views/AboutView.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>이것은 About 페이지</h1>
   </div>
 </template>
+<script>
+export default {
+  name: 'AboutPage',
+  components: {},
+  data() {
+    return {
+      sampleData: ''
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
