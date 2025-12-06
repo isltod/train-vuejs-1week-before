865f6a45e92a6fe118e4e334dbfcb2314ed483da
E. K. Lee <wolf@teoal.net>
2025-12-07 03:38:06 +0900

    6장 7장 Postman Mock Server와 vue 연동 실습 완료

---
+++ vue-project/src/components/PageTitle.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h2>Page Title</h2>
+    </div>
+</template>
---
+++ vue-project/src/views/DataBindingList.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>제품명</th>
+                    <th>가격</th>
+                    <th>카테고리</th>
+                    <th>배송료</th>
+                    <th>프로덕트</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr :key="i" v-for="(product, i) in productList">
+                    <td>{{ product.product_name }}</td>
+                    <td>{{ product.price }}</td>
+                    <td>{{ product.category }}</td>
+                    <td>{{ product.delivery_price }}</td>
+                    <td>{{ product }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            productList: []
+        };
+    },
+    created() {
+        this.getList();
+    },
+    methods: {
+        async getList() {
+            this.productList = await this.$api(
+                "https://e15ad0b9-8be3-4893-b85e-69aa59e85c68.mock.pstmn.io/list", 
+                "get"
+            );
+            console.log(this.productList);
+        }
+    }
+}
+</script>
+<style scoped>
+    table {
+        font-family: Arial, Helvetica, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+    }
+    td, th {
+        border: 1px solid #dddddd;
+        text-align: left;
+        padding: 8px;
+    }
+</style>
