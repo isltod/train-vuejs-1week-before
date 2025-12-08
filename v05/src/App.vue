5d9a80ccf898f14e619257bb2ed8d3e4c33e03a2
E. K. Lee <wolf@teoal.net>
2025-12-09 00:41:07 +0900

    왕초보 홈페이지만들기 슬롯 실습 완료

---
+++ v05/src/App.vue
@@ -0,0 +1,29 @@
+<script>
+  import SlotLayout from './components/SlotLayout.vue';
+  import FancyButton from './components/FancyButton.vue';
+  export default {
+    components: {SlotLayout, FancyButton}
+  }
+</script>
+
+<template>
+  <div>
+    <SlotLayout>
+      <template v-slot:header>
+        <h1>이것은 부모 컴포넌트의 헤더</h1>
+      </template>
+      <template #default>
+        <p class="content">이것은 부모 컴포넌트의 콘텐츠, default의 경우 슬롯 컴포넌트의 기본 값은 안나온다...</p>
+      </template>
+      <template #footer></template>
+    </SlotLayout>
+    <FancyButton>
+      <span style="color: red; font-weight: bold;">부모 컴포넌트 캡션</span>
+    </FancyButton>
+    <FancyButton v-slot="{slotCaption}">
+      <span style="color: blue; font-weight: bold;">{{ slotCaption }}</span>
+    </FancyButton>
+  </div>
+</template>
+
+<style></style>
---
+++ v05/src/components/SlotLayout.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <header>
+            <slot name="header">헤더 기본 값</slot>
+        </header>
+        <main>
+            <slot>콘텐츠 기본 값</slot>
+        </main>
+        <footer>
+            <slot name="footer" :slotFooter>푸터 기본 값</slot>
+        </footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                slotFooter: "슬롯 콤포넌트가 가지고 있는 푸터 값"
+            }
+        }
+    }
+</script>
