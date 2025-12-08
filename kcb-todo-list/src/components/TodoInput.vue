513715c72b100f4ee1a6f3ebeccf88a18a3c935b
E. K. Lee <wolf@teoal.net>
2025-12-09 01:53:30 +0900

    왕초보 홈페이지만들기 할일관리 모양잡기

---
+++ kcb-todo-list/src/components/TodoInput.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <form id="todo-form">
+            <input type="text" id="todo-input" placeholder="새 할일을 입력하세요.">
+            <button>추가</button>
+        </form>
+    </div>
+</template>
---
+++ kcb-todo-list/src/components/TodoList.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <ul id="todo-list">
+            <li>
+                Sample Task 1
+                <button>완료</button>
+            </li>
+        </ul>
+    </div>
+</template>
