b5e19277f8185ef760a1a21081285ea2b954258e
E. K. Lee <wolf@teoal.net>
2025-12-09 14:45:08 +0900

    왕초보 홈페이지만들기 할일관리 실습 완료

--- kcb-todo-list/src/components/TodoList.vue
+++ kcb-todo-list/src/components/TodoList.vue
@@ -1,10 +1,21 @@
 <template>
     <div>
         <ul id="todo-list">
-            <li>
-                Sample Task 1
-                <button>완료</button>
+            <li v-for="todo in todoList" :key="todo.id" :class="{'is-done': todo.isDone}">
+                {{ todo.content }}
+                <button @click="updateTodo(todo.id)">완료</button>
             </li>
         </ul>
     </div>
 </template>
+
+<script>
+    export default {
+        props: ["todoList"],
+        methods: {
+            updateTodo(id) {
+                this.$emit("updateTodo", id);
+            }
+        }
+    }
+</script>
