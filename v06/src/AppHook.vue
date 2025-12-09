b42371e20b417bd9ed05b3f3be734d2372266d81
E. K. Lee <wolf@teoal.net>
2025-12-09 19:30:00 +0900

    왕초보 홈페이지만들기 컴포지션 API 기본 문법 실습 완료

---
+++ v06/src/AppHook.vue
@@ -0,0 +1,26 @@
+<script>
+import { computed, ref } from 'vue';
+
+  export default {
+    setup() {
+      const myNum = ref(0);
+      const increase1 = () => myNum.value++;
+      const oddEven = computed(
+        () => (myNum % 2 == 0) ? "짝수" : "홀수"
+      );
+
+      return {myNum, increase1, oddEven};
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <h3>홀짝 판별 Options API</h3>
+    <p>{{ myNum }}</p>
+    <p>홀짝 판별: {{ oddEven }}</p>
+    <button @click="increase1">1씩 증가</button>
+  </div>
+</template>
+
+<style></style>
---
+++ v06/src/AppOptions.vue
@@ -0,0 +1,30 @@
+<script>
+  export default {
+    data() {
+      return {
+        myNum: 0
+      }
+    },
+    computed: {
+      oddEven() {
+        return (this.myNum % 2 == 0) ? "짝수" : "홀수"
+      }
+    },
+    methods: {
+      increase1() {
+        this.myNum++;
+      }
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <h3>홀짝 판별 Options API</h3>
+    <p>{{ myNum }}</p>
+    <p>홀짝 판별: {{ oddEven }}</p>
+    <button @click="increase1">1씩 증가</button>
+  </div>
+</template>
+
+<style></style>
