b6b4c7bf6b4f0e8917ca2cd71f06feeaddd2db6b
E. K. Lee <wolf@teoal.net>
2025-12-09 20:01:31 +0900

    왕초보 홈페이지만들기 컴포지션 API 컴포넌트 실습 완료

---
+++ v07/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue';
+import ButtonCounter from './components/ButtonCounter.vue';
+  const title = "부모가 준 제목";
+  const content = "부모가 준 내용";
+
+  const childMessage = ref("");
+  const getChildMessage = (childData) => {
+    childMessage.value = childData;
+  }
+</script>
+
+<template>
+  <div>
+    <h3>여기는 부모 컴포넌트</h3>
+    <p>{{ childMessage }}</p>
+    <ButtonCounter class="component" :title="title" :mainContent="content" @get-child-message="getChildMessage"/>
+  </div>
+</template>
+
+<style scoped>
+  .component {
+    border: 1px solid gray;
+    padding: 2%;
+  }
+</style>
