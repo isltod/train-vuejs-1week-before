ec0030f464a0b496485fa2686d5f18f609e4134c
E. K. Lee <wolf@teoal.net>
2025-12-11 16:32:00 +0900

    왕초보 홈페이지만들기 props drilling 실습 완료

---
+++ v11/src/Child.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import GrandChild from './GrandChild.vue';
+
+</script>
+
+<template>
+    <div>
+        <h2>Child View</h2>
+        <p>부모의 숫자: </p>
+        <GrandChild />
+    </div>
+</template>
+
+<style scoped>
+    div {
+        border: 3px solid blueviolet;
+        padding: 20px;
+        margin-top: 20px;
+    }
+</style>
---
+++ v11/src/GrandChild.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { inject, ref } from 'vue';
+import { useCounterStore } from './stores/counter';
+    // provide/inject 부분
+    const num = inject("num")
+    // pinia 부분
+    const counter = useCounterStore()
+</script>
+<template>
+    <div>
+        <h3>Grand Child View</h3>
+        <p>provide/inject 조부모의 숫자: {{ num }}</p>
+        <p>pinia 조부모의 숫자: {{ counter.count }} </p>
+    </div>
+</template>
+
+<style scoped>
+    div {
+        border: 3px solid greenyellow;
+        padding: 20px;
+        margin-top: 20px;
+    }
+</style>
